1650f817e46dbbdc819804c9babc8a6d6f7091ea
Leonchikova Nina <leon4iknina@gmail.com>
2023-06-02 20:47:38 +0300

    Nina13071988

--- controller.py
+++ controller.py
@@ -1,49 +1,57 @@
-import view, model
+import os
 
-def start():
-    view.greetings()
+def add_contact():# добавление
+    name = input("Enter name: ")
+    surname = input("Enter surname: ")
+    patronymic = input("Enter patronymic: ")
+    phone_number = input("Enter phone number: ")
+    with open("phonebook.txt", "a") as f:
+        f.write(f"{name},{surname},{patronymic},{phone_number}\n")
+    print("Contact added successfully.")
+
+def search_contact():# поиск
+    search_key = input("Enter search key: ")
+    with open("phonebook.txt", "r") as f:
+        for line in f:
+            if search_key in line:
+                print(line)
+
+
+def changer_contacts():# изменение
+    with open("phonebook_changer.txt", "w") as f:
+        data = f.read()
+    with open("phonebook.txt", "a") as f:
+        f.write(data)
+    print("Contacts changeres successfully.")
+    
+def delete_contacts():
+    with open("phonebook.txt", 'r+') as f:
+        data = f.read()
+    with open("phonebook_delete.txt", 'w+') as f:
+        f.write(data)
+    print("Contacts delete successfully.")
+
+def main():
     while True:
-        view.menu()
-        answer = input('Выберите нужную вам команду и введите соответствующую цифру: ')
-        print()
-        if answer == '1':
-            data = model.storage()
-            view.show_contacts(data)
-        elif answer == '2':
-            contact = input("Введите данные, которые нужно добавить: ")
-            res = model.add_contact(contact)
-            if res: 
-                view.success(res)
-            else: 
-                view.not_success(res)
-        elif answer == '3':
-            contact = input("Введите данные для поиска: ")
-            res = model.finder(contact)
-            view.show_contacts(res)
-        elif answer == '4':
-            contact = input("Введите данные для поиска контакта, который требуется изменить: ")
-            res = model.finder(contact)
-            view.show_contacts(res)
-            contact_change = input("Введите новые данные для изменения: ")
-            res_change = model.changer(contact_change)
-            if res_change: 
-                view.success(res)
-            else: 
-                view.not_success(res)
-        elif answer == '5':
-            contact = input("Введите данные контакта, который требуется удалить: ")
-            res = model.delete_cont(contact)
-            view.show_contacts(res)
-            if res: 
-                view.success(res)
-            else: 
-                view.not_success(res)
-        elif answer == '6':
-            view.ciao()
-        else:
-            view.error()
+        print("1. Add contact")
+        print("2. Search contact")
+        print("3. Delete")
+        print("4. Changer contacts")
+        print("5. Exit")
         
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            add_contact()
+        elif choice == "2":
+            search_contact()
+        elif choice == "3":
+            delete_contacts()
+        elif choice == "4":
+            changer_contacts()
+        elif choice == "5":
+             break
+        else:
+            print("Invalid choice.")
+        input("Press Enter to continue...")
+        os.system("clear")
 
-
-# if __name__ == "__main__":
-#     start()
